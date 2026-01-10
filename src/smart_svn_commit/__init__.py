fb9393b661d89b42b7893e00630383354d5a8c6c
fangj <fangjinxx@qq.com>
2026-01-10 10:02:07 +0800

    chore: bump version to 2.3.0

--- src/smart_svn_commit/__init__.py
+++ src/smart_svn_commit/__init__.py
@@ -4,17 +4,17 @@ Smart SVN Commit - AI 驱动的 SVN 提交助手
 提供 PyQt5 GUI 界面，支持 AI 生成提交消息和 TortoiseSVN 集成。
 """
 
-__version__ = "2.2.0"
+__version__ = "2.3.0"
 __author__ = "Your Name"
 __license__ = "MIT"
 
 # 导出主要 API
-from .core.svn_executor import SVNCommandExecutor
-from .core.fs_helper import FileSystemHelper
-from .core.config import load_config, save_config, init_config
-from .core.parser import parse_svn_status
-from .core.commit import execute_svn_commit
 from .ai.factory import generate_commit_message
+from .core.commit import execute_svn_commit
+from .core.config import init_config, load_config, save_config
+from .core.fs_helper import FileSystemHelper
+from .core.parser import parse_svn_status
+from .core.svn_executor import SVNCommandExecutor
 
 __all__ = [
     "__version__",
